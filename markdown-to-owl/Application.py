030b84ffc746c2d569cc1c7730ee35df82ac7c4e
jakezion <jakesumner@live.co.uk>
2021-08-19 10:07:31 +0100

    added comments

--- markdown-to-owl/Application.py
+++ markdown-to-owl/Application.py
@@ -11,8 +11,8 @@ import os
 # from numpy.distutils.exec_command import filepath_from_subprocess_output
 
 # sbolVisualDir= "../../SBOL-visual/Glyphs"
-sbolVisualDir = "../versions/v3.0/Glyphs"  # TODO: check for updating to external resource for glyphs
-# sbolVisualDir = "https://synbiodex.github.io/SBOL-visual/Glyphs"
+# sbolVisualDir = "../versions/v3.0/Glyphs"  # TODO: check for updating to external resource for glyphs
+sbolVisualDir = "https://synbiodex.github.io/SBOL-visual/Glyphs"
 
 
 # sbolVisualDir = "../Glyphs"
@@ -115,10 +115,10 @@ def parseFiles(directory):
 
 
 identifiers = inferIdentifiers(sbolVisualDir, {})
-print("")
-print("Creating the ontology! ...")
+print("...")
+#print("Creating the ontology! ...")
 parseFiles(sbolVisualDir)
-print("saving ontology ...")
+#print("saving ontology ...")
 # saveOntology(tag)
 saveOntology()
 print("done!")
