d694006835f74936d6780ac3f2310c3c27897419
jakezion <jakesumner@live.co.uk>
2021-08-22 20:19:51 +0100

    LODE added as java application

--- lode-document-extractor/src/main/java/lodedocumentextractor.java
+++ lode-document-extractor/src/main/java/lodedocumentextractor.java
@@ -14,8 +14,6 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-// import com.github.dissys.*;
-
 import it.essepuntato.lode.MimeType;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -27,9 +25,6 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.*;
 
-//import it.essepuntato.lode.LodeServlet;
-//import it.essepuntato.lode.SourceExtractor;
-
 public class lodedocumentextractor {
 
     //private final String xsltURL = "http://lode.sourceforge.net/xslt";
@@ -40,94 +35,42 @@ public class lodedocumentextractor {
 
         System.out.println("...");
 
-        File rdf = new File("../sbol-vo.rdf");
-        File html = new File("../sbol-vo.html");
-        // Document doc = Jsoup.parse(ontology, "UTF-8");
+        File input = new File("../sbol-vo.rdf");//TODO: pass as parameter
+        File output = new File("../sbol-vo.html");//TODO: pass as parameter
 
-        // generateLodeHTMLDocument(rdf, html);
-        //File file = new File("./extraction.xsl");
-
-        String xsltURL = "../lode-document_files/extraction.xsl";
-        //File xsltURL = new File("lode-document_files/extraction.xsl");
+        String xsltURL = "../lode-document_files/extraction.xsl"; //TODO: pass as parameter
         String cssLocation = "../lode-document_files/lode_files" + File.separator;
-        // File cssLocation = new File("lode-document_files/lode_files");
-
-
-//        String xsltURL = "resources/extraction.xsl";
-//        String cssLocation = "resources/lode_files" + File.separator;
 
         SourceExtractor extractor = new SourceExtractor();
         extractor.addMimeTypes(MimeType.mimeTypes);
-        // response.setCharacterEncoding("UTF-8");
+
 
         String content = "";
-        content = extractor.exec(rdf);
+        content = extractor.exec(input);
 
         // content = LodeServlet.applyXSLTTransformation(content); //TODO: fix as necessary
 
         TransformerFactory tfactory = new net.sf.saxon.TransformerFactoryImpl();
-
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
 
         Transformer transformer = tfactory.newTransformer(new StreamSource(xsltURL));
 
         transformer.setParameter("css-location", cssLocation);
-        //transformer.setParameter("css-location", cssLocation + File.separator);
         transformer.setParameter("lang", "en");
-        transformer.setParameter("ontology-url", rdf);
-        //transformer.setParameter("source", String.format("%s%ssource", cssLocation,File.separator));
+        transformer.setParameter("ontology-url", input);
         transformer.setParameter("source", String.format("%ssource", cssLocation));
+
         StreamSource inputSource = new StreamSource(new StringReader(content));
+        transformer.transform(inputSource, new StreamResult(out));
 
-        transformer.transform(inputSource, new StreamResult(output));
+        content = out.toString();
 
-        content = output.toString();
-
-        FileWriter writer = new FileWriter(html);
+        FileWriter writer = new FileWriter(output);
         writer.write(content);
         writer.close();
 
-        // FileUtils.writeStringToFile(source, doc.outerHtml(), "UTF-8");
         System.out.println("done!");
 
     }
-
-
-    /*
-    private void generateLodeHTMLDocument(URL request, File response) throws IOException {
-        //PrintWriter out = response.getWriter();
-        SourceExtractor extractor = new SourceExtractor();
-        extractor.addMimeTypes(MimeType.mimeTypes);
-        // response.setCharacterEncoding("UTF-8");
-
-        String content = "";
-        content = extractor.exec(request);
-
-        content = applyXSLTTransformation(content); //TODO: fix as necessary
-        FileWriter writer = new FileWriter(response);
-        writer.write(content);
-    }
-     */
-
-    //private String applyXSLTTransformation(String source, String ontologyUrl) throws TransformerException {
-//    private String applyXSLTTransformation(String source) throws TransformerException {
-//        TransformerFactory tfactory = new net.sf.saxon.TransformerFactoryImpl();
-//
-//        ByteArrayOutputStream output = new ByteArrayOutputStream();
-//
-//        Transformer transformer = tfactory.newTransformer(new StreamSource(xsltURL));
-//
-//        transformer.setParameter("css-location", cssLocation);
-//        transformer.setParameter("lang", "en");
-//        // transformer.setParameter("ontology-url", ontologyUrl);
-//        transformer.setParameter("source", cssLocation + "source");
-//
-//        StreamSource inputSource = new StreamSource(new StringReader(source));
-//
-//        transformer.transform(inputSource, new StreamResult(output));
-//
-//        return output.toString();
-//    }
-
 
 }
